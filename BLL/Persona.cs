8f2611416ad44be2f68d945804f7c00ad918ac25
Enel Almonte <enelramon@gmail.com>
2015-06-04 16:04:56 -0400

    Ejemplo de Clase con  Detalle

---
+++ BLL/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public String Nombres { get; set; }
+
+        public List<Telefonos> Telefonos { get; set; }
+
+
+        public bool Insertar()
+        {
+            String Comando;
+
+            Comando = "Insert into Personas(Nombres) Values (" + this.Nombres   + ")";
+
+            foreach (Telefonos tel in Telefonos)
+            {
+                Comando += "Insert into Telefonos(IdPersona,Telefono) Values (" + this.IdPersona + ",'" + tel.Telefono  +"')";
+            }
+
+            //DAL.SaveData(Comando);
+
+            return true;
+        }
+
+        public void AgregarTelefono(int tipo, String telefono)
+        {
+
+            this.Telefonos.Add(new Telefonos(tipo, telefono));
+        }
+    }
+}
---
+++ BLL/Telefonos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class Telefonos
+    {
+        public int Tipo { get; set; }
+        public String Telefono { get; set; }
+
+        public Telefonos(int tipo,String telefono)
+        {
+            this.Tipo = tipo;
+            this.Telefono = telefono;
+
+        }
+    }
+}
